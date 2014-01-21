4d9cdb3842dcbc2d7a5768ed0051d2dc462b506d
Marco <marco.a.cerna@gmail.com>
2014-01-20 23:28:51 -0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HowManyClicks::Application.config.secret_token = 'ada6e7a51e7d54c0c13a942991d44e02884d758ba34fcfce71f21236acb65a6e4c15a085eb631973bfc11ce7e9fc1fb4aa4b69e42021cb0b8b23245fd5d74cf6'
